65bc4bfea98fbab16869a4e5678b85f48e1f4f89
huynhduydong <hddongqn@gmail.com>
2024-05-28 12:47:56 +0700

    feat(payment): update reicept after payment #19

--- backend/src/main/java/com/dong/service/impl/ReceiptServiceImpl.java
+++ backend/src/main/java/com/dong/service/impl/ReceiptServiceImpl.java
@@ -31,6 +31,11 @@ public class ReceiptServiceImpl implements ReceiptService {
         return this.receiptRepo.getReceiptById(id);
     }
 
+    @Override
+    public boolean addOrUpdateReceipt(Receipt r) {
+        return this.receiptRepo.addOrUpdateReceipt(r);
+    }
+
 //    @Override
 //    public List<ReceiptDTO> getReceiptDetail(Map<String, String> params) {
 //        return this.receiptRepo.getReceiptDetail(params);
