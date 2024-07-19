e85d5b5a9391d37a352ad89d097ee94002d7b968
maverick <trakimbang0809@gmail.com>
2024-07-19 22:59:46 +0700

    chore: mv ./staging to infrastructure/stage

--- staging/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "terraform-state-maverick"
-    key     = "terraform/backend"
-    region  = "ap-southeast-1"
-    profile = "maverick"
-  }
-}
--- staging/providers.tf
+++
@@ -1,4 +0,0 @@
-provider "aws" {
-  region  = var.REGION
-  profile = "maverick"
-}
